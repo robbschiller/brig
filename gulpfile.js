0c89c5967a19bc9b7890655f9fd487d4d5e1f270
Robb Schiller <robbschiller@gmail.com>
2014-11-18 15:16:27 -0500

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,19 @@
 'use strict';
 
 var gulp = require('gulp');
-
 var uglify = require('gulp-uglify');
-var sass = require('gulp-ruby-sass');
+var sass = require('gulp-sass');
 var browserify = require('gulp-browserify');
 
 var paths = {
   browserify: {
-    in: 'public/js/human.so.js',
-    out: 'public/dist/js/human.so.js',
+    in: 'public/js/app.js',
+    out: 'public/dist/js/app.js',
     watch: 'public/js/**/*.js',
     options: {},
   },
   sass: {
-    in: 'public/sass/main.scss',
+    in: 'public/sass/app.scss',
     out: 'public/css',
     watch: 'public/sass/**/*.scss',
     options: {},
